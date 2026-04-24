6b927354c48650bc96d56055d51aac6380372e65
Deeksha Nage <163334017+Deeksha11n@users.noreply.github.com>
2026-04-24 22:57:04 +0530

    Add files via upload

---
+++ sum_element.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[ ]= {10,20,20,30,48};
+    int = sizeof(arr)/sizeof(arr[0])
+    int sum =0;
+    for(int i =0;i<n;i++){
+        sum = sum+ arr[i];
+    }
+    cout<<sum;
+    return 0;
+}
